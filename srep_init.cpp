3f67049699494d1a4eec45f53d71b1361368ef93
Meghan Stuart <mstuart@midag29.cs.unc.edu>
2019-04-09 11:47:41 -0400

    Correction to setting elasticity

--- srep_init.cpp
+++ srep_init.cpp
@@ -1229,7 +1229,7 @@ int srep_init::backward_flow()
     vtkSmartPointer<vtkThinPlateSplineTransform> tps = vtkSmartPointer<vtkThinPlateSplineTransform>::New();
     tps->SetSourceLandmarks(source_landmarks);
     tps->SetTargetLandmarks(target_landmarks);
-    tps->SetSigma(0.05);
+    tps->SetSigma(this->elasticity);
     tps->SetBasisToR();
 
     vtkSmartPointer<vtkTransformPolyDataFilter> transformFilter =
